d9c536317966a9a8f2ed5084e9ab5d12178e2664
Mathias Wagner <germannewsmaker@gmail.com>
2026-09-22 23:30:30 +0200

    Bootstrap client vite project

---
+++ client/vite.config.js
@@ -0,0 +1,28 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import * as path from "path";
+
+export default defineConfig({
+    base: "/@tunlit/",
+    plugins: [react()],
+    css: {
+        preprocessorOptions: {
+            sass: {
+                api: "modern"
+            }
+        }
+    },
+    resolve: {
+        alias: {
+            "@": path.resolve(__dirname, "src"),
+        }
+    },
+    server: {
+        proxy: {
+            "/@tunlit/api": {
+                target: "http://localhost:8080",
+                ws: true
+            }
+        }
+    }
+});
